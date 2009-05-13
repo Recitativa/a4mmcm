b4dd91fdea865c1f83863fa12fb5fa7f0290863c
hoxide <hoxide@44aea711-dc27-0410-a143-5778ce1304a0>
2009-05-13 08:27:31 +0000

    aa

---
+++ trunk/quantile/theoretic.c
@@ -0,0 +1,14 @@
+#include <gsl/gsl_cdf.h>
+
+/*
+  Ref: Angelos Dassios, The Distribution of the quantile of a brownian motion with drift and the pricing of related path-dependent options, The annals of Applied Probability, 1995, Vol. 5, No. 2, 389-298
+ */
+
+double 
+  if(x >0)
+    {
+      return 1/sigma
+    }
+  else 
+    return 0
+
