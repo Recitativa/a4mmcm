6feb187f5acba353aae05fd59c7c31e47c880e8c
hoxide <hoxide@44aea711-dc27-0410-a143-5778ce1304a0>
2009-05-14 05:49:40 +0000

    works

--- quantile/theoretic.cpp
+++ quantile/theoretic.cpp
@@ -3,54 +3,41 @@
 #include <iostream>
 
 
-double Quatile::g(double x, double T) {
-  double result;
+
+
+double Quantile::dX(double x)  {
+  double params[] =  {sigma, alpha};
+  return dX_i(x, params);}
+
+double Quantile::dX_i(double x, void * Iparams) {
+  double * params = (double *)Iparams;
+  double sigma = params[0];
+  double alpha = params[1];
+  double beta = sqrt((1.-alpha)/alpha);
+  static const double c1 = sqrt(2/PI)*2;
+
+  if(x >=0) 
+    return c1 * exp(-x*x/2) * gsl_cdf_ugaussian_Q(beta * x);
+  else
+    return c1 * exp(-x*x/2) * gsl_cdf_ugaussian_Q(-x/beta);
+}
+
+double Quantile::mean() {
   gsl_integration_workspace * w 
     = gsl_integration_workspace_alloc (1000);
-  result = g_inn(x,T, sigma, alpha, w);
-  gsl_integration_workspace_free (w);
-  return result;
-}
-
-inline double \
-Quatile::g_inn(double x, double T, double sigma, double alpha,		\
-	       gsl_integration_workspace *w) {
   
   double result, error;
-  double Ialpha = 1.0;
-  
-  double params[] = {x, T, sigma, alpha};
+  double params[] = {sigma, alpha};
   
   gsl_function F;
-  
-  std::cout << intg(1,params) << std::endl;
-  F.function = &intg;
+  F.function = &XdX_i;
   F.params = params;
   
-  gsl_integration_qags (&F, 0, 1, 0, 1e-7, 1000,
+  gsl_integration_qagi (&F, 1e-7, 1e-5, 1000,
 			w, &result, &error); 
+  gsl_integration_workspace_free(w);
+  return result;
+
+  
   return result;
 }
-// g_1(x-y;\alpha*t)*g_2(y;(1-\alpha)*t)
-inline double Quatile::g1(double x,double t, double sigma) {
-  if(x > 0)
-    return 1/sigma * sqrt(2/PI*t)*exp(-x*x/2/(sigma*sigma)/t);
-  else 
-    return 0;
-}
-inline double Quatile::g2(double x, double t, double sigma) {
-  return g1(-x,t, sigma);
-}
-inline double Quatile::intg(double y, void * Iparams) {
-  double * params = (double *) Iparams;
-  double x = params[0];
-  double T = params[1];
-  double sigma = params[2];
-  double alpha = params[3];
-  std::cout <<x <<T << sigma << alpha << std::endl;
-  double f = g1(x-y,alpha*T, sigma)*g2(x, (1-alpha)*T, sigma);
-  std::cout << f << std::endl;
-  return f;
-}
-
-
