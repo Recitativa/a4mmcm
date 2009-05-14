d21a61080e4f20e3cc1c14913a55394bdb7acb60
hoxide <hoxide@44aea711-dc27-0410-a143-5778ce1304a0>
2009-05-14 03:22:38 +0000

    stupid fault

--- trunk/quantile/theoretic.cpp
+++ trunk/quantile/theoretic.cpp
@@ -1,46 +1,56 @@
 #include "theoretic.hpp"
 #include <cmath>
-#include <gsl/gsl_cdf.h>
-#include <gsl/gsl_integration.h>
+#include <iostream>
 
-// Ref: Angelos Dassios, The Distribution of the quantile of a
-// brownian motion with drift and the pricing of related
-// path-dependent options, The annals of Applied Probability, 1995,
-// Vol. 5, No. 2, 389-298
 
-class Quatile 
-{
-public:
-  double sigma;
-  double alpha;
-  double T;
-  Quatile(double Isigma = 1, double Ialpha=1, double IT=1) 
-    : sigma(Isigma), alpha (Ialpha), T (IT) {}
-  double g(double x) {
-    gsl_integration_workspace * w 
-      = gsl_integration_workspace_alloc (1000);
-    
-    double result, error;
-    double expected = -4.0;
-    double Ialpha = 1.0;
-    
-    gsl_function F;
-    F.function = &p;
-    F.params = &Ialpha;
-    
-    gsl_integration_qags (&F, 0, 1, 0, 1e-7, 1000,
-			  w, &result, &error); 
-  }
-  
-private:
-  // g_1(x-y;\alpha*t)*g_2(y;(1-\alpha)*t)
-  double g1(double x) {
-    if(x > 0)
-      return 1/sigma * sqrt(2/PI*T)*exp(-x*x/2/(sigma*sigma)/t);
-    else 
-      return 0;
-  }
-  double g2(double x) {
-    return g1(-x);
-  }
+double Quatile::g(double x, double T) {
+  double result;
+  gsl_integration_workspace * w 
+    = gsl_integration_workspace_alloc (1000);
+  result = g_inn(x,T, sigma, alpha, w);
+  gsl_integration_workspace_free (w);
+  return result;
 }
+
+inline double \
+Quatile::g_inn(double x, double T, double sigma, double alpha,		\
+	       gsl_integration_workspace *w) {
+  
+  double result, error;
+  double Ialpha = 1.0;
+  
+  double params[] = {x, T, sigma, alpha};
+  
+  gsl_function F;
+  
+  std::cout << intg(1,params) << std::endl;
+  F.function = &intg;
+  F.params = params;
+  
+  gsl_integration_qags (&F, 0, 1, 0, 1e-7, 1000,
+			w, &result, &error); 
+  return result;
+}
+// g_1(x-y;\alpha*t)*g_2(y;(1-\alpha)*t)
+inline double Quatile::g1(double x,double t, double sigma) {
+  if(x > 0)
+    return 1/sigma * sqrt(2/PI*t)*exp(-x*x/2/(sigma*sigma)/t);
+  else 
+    return 0;
+}
+inline double Quatile::g2(double x, double t, double sigma) {
+  return g1(-x,t, sigma);
+}
+inline double Quatile::intg(double y, void * Iparams) {
+  double * params = (double *) Iparams;
+  double x = params[0];
+  double T = params[1];
+  double sigma = params[2];
+  double alpha = params[3];
+  std::cout <<x <<T << sigma << alpha << std::endl;
+  double f = g1(x-y,alpha*T, sigma)*g2(x, (1-alpha)*T, sigma);
+  std::cout << f << std::endl;
+  return f;
+}
+
+
