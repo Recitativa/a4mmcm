4997c1d582797bf51e8915fbbb018a4eca2c6d11
hoxide <hoxide@44aea711-dc27-0410-a143-5778ce1304a0>
2009-05-24 06:29:03 +0000

    new apporch by 2-quantile

--- quantile/simquantile.cpp
+++ quantile/simquantile.cpp
@@ -8,10 +8,7 @@ using namespace std;
 
 int main() {
   BrownSim S;
-  double RQuantile[] = {8./16, 9./16, 10./16, 11./16,12./16,13./16,14./16,
-			15./16, 16./16};
-  const int nRQ = sizeof(RQuantile) / sizeof(double); // #of Quantiles
-
+  
   // int BrownSim::Sim(double T=t1, const int P2=25, const int Terms=100,
   // 		  const int Rb=4, // records begin with 2^Rb+1 points. 
   // 		  const int Re = P2-5, // records end with 2^Re+1 points.
@@ -21,14 +18,12 @@ int main() {
   // 		  ) 
 
   SimPara Para;
-  Para.nRQ = nRQ;
-  Para.RQuantile = RQuantile;
   Para.T = 1;
   Para.P2 = 22;
-  Para.Terms = 100;
+  Para.Terms = 500;
   Para.Rb = 4;
   Para.Re = Para.P2-5;
-  Para.Nseg = 22;
+  Para.Nseg = 26;
   Para.Rseed = 123;
   S.Sim(Para);
 }
