9c13d93a452548e91fcaa0960e80ce290e6f69c3
hoxide <hoxide@44aea711-dc27-0410-a143-5778ce1304a0>
2009-09-09 04:19:57 +0000

    change2

--- trunk/quantile/inv_q.r
+++ trunk/quantile/inv_q.r
@@ -9,26 +9,27 @@ Pr <- function(t, x, sigma, mu) {
 }
 
 
+
 ## main function to treat the data
 run <- function(T = .25, alpha=.8, sigma=.2, r=.5, S=100, K=95, n=1000) {
   x <- seq(0,3,length.out=500);
-  yy <- seq(0,1, length.out=100);
+  yy <- seq(0,1, length.out=1000);
   mu <- (r-sigma^2/2)
   
   PM1 <- Pr(alpha*T, x, sigma, mu)
   PM2 <- Pr((1-alpha)*T, x, sigma,mu)
   
-  invPM1 <- approxfun(PM1,x,method='constant',)
-  invPM2 <- approxfun(PM2,x,method='constant')
+  invPM1 <- smooth.spline(PM1,x)
+  invPM2 <- smooth.spline(PM2,x)
   plot(PM1,x,type='l',col='red');
   lines(PM2,x,type='l',col='blue');
-  inM1 <- invPM1(yy)
-  inM2 <- invPM2(yy)
+  inM1 <- predict(invPM1, yy)$y
+  inM2 <- predict(invPM2, yy)$y
   lines(yy,inM1,col='green');
   lines(yy,inM2,col='green');
   
-  simM1 <- invPM1(runif(n))
-  simM2 <- invPM1(runif(n))
+  simM1 <- predict(invPM1,runif(n))$y
+  simM2 <- predict(invPM2,runif(n))$y
   
   Sat <- S*exp(simM1-simM2)
   V <- 0;
