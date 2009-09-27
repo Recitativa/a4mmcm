77093a610dc473280e65455fabe576a74cd1d476
hoxide <hoxide@44aea711-dc27-0410-a143-5778ce1304a0>
2009-09-27 07:17:19 +0000

    European Option

--- trunk/quantile/bitree_rec.cpp
+++ trunk/quantile/bitree_rec.cpp
@@ -91,11 +91,11 @@ Real pprice(int steps, Real ZZ) {
     assert(NTERM.qth==(int)(alpha*nstack));				\
     NTERM.qu_it = it;							\
     Real Z1 = *it, Z2 = *(++it);					\
-    /*   NTERM.Walpha = Z1 + (Z2-Z1)*(nstack*alpha-qth);*/		\
+    NTERM.Walpha = Z1 + (Z2-Z1)*(nstack*alpha-qth);			\
     /*NTERM.g1 = payfun(NTERM.Walpha);*/				\
-    NTERM.Walpha = *it;							\
+    /*NTERM.Walpha = *it;*/						\
     if(nstack==steps-1) NTERM.g1 = payfun(NTERM.Walpha);       		\
-    else NTERM.g1=1000000;						\
+    else NTERM.g1=10000;						\
     nstack++;								\
     /*PRINT_LIST;*/							\
   } while(false)
@@ -129,63 +129,26 @@ Real pprice(int steps, Real ZZ) {
   return pay;
 }
 
-typedef struct {
-  Real h;
-  Real A;
-} RichNode;
-
-Real richardson_extrapolation(RichNode * data, int len) {
-  RichNode *S = new RichNode[len];
-  int i,j,k;
-  for(i=0;i<len;i++) S[i] = data[i];
-
-  for(k=1,j=len;j>=2;j--,k++) {
-    for(i=0;i<j-1;i++) {
-      Real t = S[i].h/S[i+1].h;
-      t = pow(t,k);
-      S[i].A = (t*S[i+1].A-S[i].A)/(t-1);
-      cerr << S[i].A << " ";
-    }
-    cerr << endl;
-  }
-  return S[0].A;
-}
-
-
 int main()
 {
   Real S0, K, r, sigma,T, mu;
   int n,i;
   S0=100, K=95, alpha=0.8, r=0.05, sigma=0.2, T=.25, mu=r-sigma*sigma/2;
   n = 20;
-  
-  RichNode R[100];
-  R[0].h = .025, R[0].A=10.2429;
-  R[1].h = 0.0208333, R[1].A=9.79268;
-  R[2].h = 0.0178571, R[2].A=9.48657;
-  R[3].h = 0.015625, R[3].A=9.45113;
-  R[4].h = 0.0138889, R[4].A=9.19926 ;
-  cerr << richardson_extrapolation(R,5) << endl;;
 
-  return 0;
-  
-
-  for(i=0;i<5;i++) {
-    n= 10+ i*2;
-
-    dt = T/n;
-    su = mu*dt;// su=.5;
-    sdelta= sigma*sqrt(dt); //sdelta=1;
-    dis = exp(-r*dt);
-    rho = S0/K;
-    cout<< su << " " << sdelta <<" "<< alpha << " " << dis <<" " << rho << endl;
-    OrderedPath.clear();
-    R[i].h = dt;
-    R[i].A = K*pprice(n+1,0);
-    cout << dt <<" " << n << " " << R[i].A << endl;
+  for(i=0;i<5; i++) {
+    n=10+2*i;
+  dt = T/n;
+  su = mu*dt;// su=.5;
+  sdelta= sigma*sqrt(dt); //sdelta=1;
+  dis = exp(-r*dt);
+  rho = S0/K;
+  cout<< su << " " << sdelta <<" "<< alpha << " " << dis <<" " << rho << endl;
+  OrderedPath.clear();
+  Real A = K*pprice(n+1,0);
+  cout << dt <<" " << n << " " << A << endl;
   }
-  cerr << "RICH" << endl;
-  cout << richardson_extrapolation(R,i) << endl;
+
   return 0;
 }
 
